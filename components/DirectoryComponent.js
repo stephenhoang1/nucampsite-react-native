9711a422e698b8aa2bf97e11f98edabb38f7592c
stephenhoang1 <s.hoang17@gmail.com>
2020-08-08 20:39:30 -0700

    Components Part 2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -7,10 +7,11 @@ function Directory(props) {
     const renderDirectoryItem = ({item}) => {
         return (
             <ListItem
-                title={item.name}
-                subtitle={item.description}
-                leftAvatar={{ source: require('./images/react-lake.jpg')}}
-            />
+            title={item.name}
+            subtitle={item.description}
+            onPress={() => props.onPress(item.id)}
+            leftAvatar={{ source: require('./images/react-lake.jpg')}}
+        />
         );
     };
 
